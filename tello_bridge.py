278b3959c3d8759bf6542560d3c1badf8e72a0ff
LGP <lgp@hibox.fr>
2022-05-14 16:55:46 +0200

    add Bridge class

---
+++ tello_bridge.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Use the sounddevice module
+# http://python-sounddevice.readthedocs.io/en/0.3.10/
+
+import time
+
+class TelloBridge():
+	def __init__(self):
+		self.changed_beat = 0
+
+
+if __name__ == '__main__':
+	td = TelloBridge()
